77a436d1f2997820a12dc8cad815345ca9053c1a
karakusHalil <karakus_halil@outlook.com>
2025-08-22 17:11:19 +0300

    Add Link

---
+++ nextjs14/app/catch/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Catch = () => {
+  return <div>Catch</div>;
+};
+
+export default Catch;
--- nextjs14/app/page.tsx
+++ nextjs14/app/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <div className="bg-red-200">Home Page</div>
+
+      <div>Home</div>
+
+      <div className="flex gap-8">
+        <Link href="/">Home</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/doc">Doc</Link>
+        <Link href="/doc/routing">Routing Doc</Link>
+        <Link href="/doc/routing/menu1">Routing Menu1</Link>
+        <Link href="/doc/routing/menu2">Routing Menu2</Link>
+        <Link href="/catch/a/b/c/d/e">Catch All</Link>
+      </div>
     </>
   );
 }
