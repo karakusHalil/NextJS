d798c95bbba78e92f6898a7e34ea6eebe879d91d
karakusHalil <karakus_halil@outlook.com>
2025-08-25 17:58:04 +0300

    Add shadcn button and checkbox

--- nextjs15/app/page.tsx
+++ nextjs15/app/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div>aaa</div>
+      <div>
+        <Button variant="default">Test</Button>
+        <button>Test2</button>
+        <Checkbox/>
+      </div>
     </>
   );
 }
