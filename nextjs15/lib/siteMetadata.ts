e8cd90ecc413b76ab1a88644a840127281cd670d
karakusHalil <karakus_halil@outlook.com>
2025-08-28 19:01:47 +0300

    Add metada

---
+++ nextjs15/lib/siteMetadata.ts
@@ -0,0 +1,19 @@
+export const siteMetadata = {
+  title: "Next.js Blog Marketplace Optimized Seo ",
+  author: "Halil İbrahim Karakuş",
+  headerTitle: "Next.js 15",
+  description: "A blog created with Next.js, Tailwind.css .",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://www.https://github.com/karakusHalil.com", // your website URL
+  siteLogo: "/file.svg",
+  socialBanner: "/HondaCivic.jpg", // add social banner in the public folder
+  email: "halilkkus42@gmail.com",
+  github: "https://github.com/karakusHalil",
+  twitter: "",
+  facebook: "",
+  youtube: "",
+  linkedin: "",
+  dribbble: "",
+  locale: "en-US",
+};
