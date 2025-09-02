66206d7044a5b7e93ce09a39c20b111b7427c173
karakusHalil <karakus_halil@outlook.com>
2025-09-02 17:57:09 +0300

    Add slider

--- nextjs15-TravelSite/travel/app/(routes)/(home)/page.tsx
+++ nextjs15-TravelSite/travel/app/(routes)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import RecentProduct from "@/components/sections/recent/RecentProduct";
+import Hero from "./_components/Hero/Hero";
 
 export default function Home() {
   return (
     <>
       <div>
+        <Hero />
         <RecentProduct />
       </div>
     </>
--- nextjs15-TravelSite/travel/app/constants/index.ts
+++ nextjs15-TravelSite/travel/app/constants/index.ts
@@ -113,3 +113,34 @@ export const packages: Packages[] = [
     discount: "14% Off",
   },
 ];
+
+export interface Slider {
+  image: string;
+  title: string;
+  description: string;
+  buttonText: string;
+}
+
+export const slides: Slider[] = [
+  {
+    image: "/slider/1.png",
+    title: "Uncovering Stories, One Trip At A Time ",
+    description:
+      "Cras ultricies ligula sed magna dictum porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+    buttonText: "READ MORE",
+  },
+  {
+    image: "/slider/2.png",
+    title: "Explore The World, One Destination At A Time",
+    description:
+      "Nulla porttitor accumsan tincidunt. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+    buttonText: "DISCOVER NOW",
+  },
+  {
+    image: "/slider/3.png",
+    title: "Find Your Next Adventure Explore The World",
+    description:
+      "Donec sollicitudin molestie malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.",
+    buttonText: "START YOUR JOURNEY",
+  },
+];
