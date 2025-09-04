45943983851b694cc7b2a9b4db0748627804fd03
karakusHalil <karakus_halil@outlook.com>
2025-09-04 14:24:29 +0300

    Add sectionOne and sectionTwo

---
+++ nextjs15-TravelSite/travel/app/(routes)/(home)/_components/Section/SectionTwo.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SectionTwo = () => {
+  return (
+    <>
+      <div className="bg-orange-100 mt-12">
+        <div className="mx-auto container text-center py-16 lg:px-28">
+          <h2 className="text-2xl lg:text-4xl font-bold text-blue-600 mb-4">
+            WELCOME TO TRENDY TRAVEL
+          </h2>
+          <div className="flex justify-center items-center mb-6">
+            <hr className="border-gray-300 w-1/5" />
+            <span className="mx-3 text-gray-400 text-xl">&#128064;</span>
+            <hr className="border-gray-300 w-1/5" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SectionTwo;
--- nextjs15-TravelSite/travel/app/(routes)/(home)/page.tsx
+++ nextjs15-TravelSite/travel/app/(routes)/(home)/page.tsx
@@ -1,13 +1,16 @@
 import RecentProduct from "@/components/sections/recent/RecentProduct";
 import Hero from "./_components/Hero/Hero";
 import SectionOne from "./_components/Section/SectionOne";
+import SectionTwo from "./_components/Section/SectionTwo";
 
 export default function Home() {
   return (
     <>
       <div>
         <Hero />
-        <SectionOne/>
+        <div className="min-h-24"></div>
+        <SectionOne />
+        <SectionTwo />
         <div className="min-h-96"></div>
         <RecentProduct />
         <div className="min-h-96"></div>
